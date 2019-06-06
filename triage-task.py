9a237ca39e5f07d9ee59325c7aa36071227d3aed
Rafael Magalhaes <rafaelmagftw@gmail.com>
2019-06-06 19:15:04 +0100

    Organizado o ficheiro script e adicionados outputs

--- triage-task.py
+++ triage-task.py
@@ -2,78 +2,75 @@
 # coding: utf-8
 
 # ## Triage Task
+#######################################################
+'''
+Utilizar o script de teste para validar a rede.
+Reduzir o numero máximo de tokens num documento:
+    - Retirar palavras mais comuns
 
-# DIR = '/home/a75377/biocreative-vi-track4/'
+'''
+#######################################################
 
-# ### Import pre-trained Word2Vec vector space
+
+# ### Imports
 
 import gensim
 from gensim.models import KeyedVectors
 
-# wv_from_bin = gensim.models.KeyedVectors.load_word2vec_format('wikipedia-pubmed-and-PMC-w2v.bin', binary = True)
-wv_from_bin = gensim.models.KeyedVectors.load_word2vec_format('PubMed-and-PMC-w2v.bin', binary = True)
-print('Pubmed w2v model loaded.')
-
-
-# ### Import training set file
-
-import json
-import numpy as np 
+import numpy as np
+import json 
 import pandas as pd 
 from pandas.io.json import json_normalize
 
-with open('PMtask_Triage_TrainingSet.json') as json_file:
-    data = json.load(json_file)
-  
-documents = json_normalize(data['documents'])
+from keras.preprocessing.text import text_to_word_sequence
 
-# ### Unpack the passages column into a standalone dataframe
+from keras.models import Model
+from keras import layers
 
-passages = json_normalize(data = data['documents'], record_path = 'passages', record_prefix = 'passage.', meta = 'id')
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
-types = json_normalize(passages['passage.infons'], meta = 'id')
+# ### Global variables
 
+W2V_FILE = 'Pubmed-and-PMC-w2v.bin'
+W2V_LIMIT = 10000
+TRAINSET_FILE = 'PMtask_Triage_TrainingSet.json'
+TESTSET_FILE = 'PMtask_Triage_TestSet.json'
+SAVE_FIG_FILE = None
 
-# ### Merge
+# ### Functions
 
-documents = documents.merge(passages, on = "id", how = "inner")
-documents = documents.merge(types, left_index = True, right_index = True)
-documents = documents.drop(columns = ['passages', 'relations', 'passage.annotations', 'passage.infons', 'passage.relations', 'passage.sentences'])
-documents['infons.relevant'].replace('no', 0, inplace = True)
-documents['infons.relevant'].replace('yes', 1, inplace = True)
-documents.name = 'training'
-
-
-# ### Import test set file
-
-with open('PMtask_Triage_TestSet.json') as json_file:
-    data_test = json.load(json_file)
+def load_pretrained_w2v(file, limit_words=None):
+    wv_from_bin = gensim.models.KeyedVectors.load_word2vec_format(file,
+                                                                  limit=limit_words, 
+                                                                  binary = True)
+    return wv_from_bin
     
-documents_test = json_normalize(data_test['documents'])
-passages_test = json_normalize(data = data_test['documents'], record_path = 'passages', record_prefix = 'passage.', meta = 'id')
-types_test = json_normalize(passages_test['passage.infons'], meta = 'id')
-
-documents_test = documents_test.merge(passages_test, on = "id", how = "inner")
-documents_test = documents_test.merge(types_test, left_index = True, right_index = True)
-documents_test = documents_test.drop(columns = ['passages', 'relations', 'passage.annotations', 'passage.infons', 'passage.relations', 'passage.sentences'])
-documents_test['infons.relevant'].replace('no', 0, inplace = True)
-documents_test['infons.relevant'].replace('yes', 1, inplace = True)
-documents_test.name = 'test'
+def load_dataset(file):
+#     Import file
+    with open(file) as json_file:
+        data = json.load(json_file)
+    documents = json_normalize(data['documents'])
+#     Unpack the passages column into a standalone dataframe
+    passages = json_normalize(data = data['documents'], record_path = 'passages', record_prefix = 'passage.', meta = 'id')
+#     Unpack the passage.infons column into a standalone dataframe
+    types = json_normalize(passages['passage.infons'], meta = 'id')
+#     Merge
+    documents = documents.merge(passages, on = "id", how = "inner")
+    documents = documents.merge(types, left_index = True, right_index = True)
+    documents = documents.drop(columns = ['passages', 'relations', 'passage.annotations', 'passage.infons', 'passage.relations', 'passage.sentences'])
+    documents['infons.relevant'].replace('no', 0, inplace = True)
+    documents['infons.relevant'].replace('yes', 1, inplace = True)
+    return documents
 
 def concat_text(dataframe):
     d = {
         'passage.text':[],
         'infons.relevant':[]
     }
-    # temp = ''
     title_count = 0
     abst_count = 0
     for index, row in dataframe.iterrows():
-    #     if index > 0 and index % 2 == 0:
-    #         d['passage.text'].append(temp)
-    #         d['infons.relevant'].append(row['infons.relevant'])
-    #         temp = ''
-    #     temp += row['passage.text']
         if row['type'] == 'title':
             temp = ''
             temp += row['passage.text']
@@ -84,139 +81,149 @@ def concat_text(dataframe):
             d['infons.relevant'].append(row['infons.relevant'])
             abst_count += 1
 
-    print(title_count)
-    print(abst_count)
+    print("Number of titles encountered:",title_count)
+    print("Number of abstracts encountered:",abst_count)
     df = pd.DataFrame(data=d)
     return df
 
-### Concat the title and abstract
-
-new_documents = concat_text(documents)
-new_documents.name = 'training'
-
-new_documents_test = concat_text(documents_test)
-new_documents_test.name = 'test'
-
-### Check document size
-# max(documents.astype('str').applymap(lambda x: len(x)).max())
-# max(documents_test.astype('str').applymap(lambda x: len(x)).max())
-
-maxlen = 3900
-
-
-# ### Text to word sequence (embedding)
-
-from keras.preprocessing.text import text_to_word_sequence
-
+# Text to word sequence
 def vectorize(row, text, embedding_matrix):
     for index, word in enumerate(text):
         try:
             embedding_matrix[row][index] = wv_from_bin.wv.vocab[word].index
         except:
             pass
+
+def word_sequence(df, shape):
+    embedding_matrix = np.ones(shape)
+    temp_df = pd.DataFrame()
+    temp_df['passage.text'] = df['passage.text'].apply(lambda x: text_to_word_sequence(x, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = False, split=' '))
+    for index, row in temp_df.iterrows():
+        vectorize(index, row['passage.text'], embedding_matrix)
+    return embedding_matrix
+
+def train_test_split(train_embedding, test_embedding, corpus, corpus_test, validation_size=0):
+    # Training data
+    X_train = train_embedding
+    y_train = corpus['infons.relevant'].values
+    
+    # Test data
+    X_test = test_embedding
+    y_test = corpus_test['infons.relevant'].values
+    
+    if(validation_size > 0):
+        # Training data
+        X_validation = X_train[-validation_size:]
+        X_train = X_train[:-validation_size]
         
-embedding_matrix_train = np.zeros((4080, maxlen))
-embedding_matrix_test = np.zeros((1427, maxlen))
+        y_validation = y_train[-validation_size:]
+        y_train = y_train[:-validation_size]
 
-def word_sequence(df):
-    df['passage.text'] = df['passage.text'].apply(lambda x: text_to_word_sequence(x, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = False, split=' '))
-    for index, row in df.iterrows():
-        if(df.name == 'training'):
-            vectorize(index, row['passage.text'], embedding_matrix_train)
-        if(df.name == 'test'):
-            vectorize(index, row['passage.text'], embedding_matrix_test)
+        return X_train, X_validation, X_test, y_train, y_validation, y_test
+    return X_train, X_test, y_train, y_test
+
+def plot_history(history, file_name=None):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    if(file_name != None):
+        plt.savefig(file_name)
+    
+def build_compile_model_Dense(input_shape):
+
+    inputs = layers.Input(shape=input_shape)
+    embedding = w2v_embedding(inputs)
+    flatten = layers.Flatten()(embedding)
+    dense = layers.Dense(10, activation = 'relu')(flatten)
+    output = layers.Dense(1, activation = 'sigmoid')(dense)
+
+    model = Model(inputs=inputs, outputs=output)
+
+    model.compile(optimizer = 'adam',
+                  loss = 'binary_crossentropy',
+                  metrics = ['accuracy'])
+    return model
+
+def fit_model(model, X_train, y_train, x_validation, y_validation, epochs, batch_size, verbose=False):
+    history = model.fit(X_train, y_train,
+                        epochs = epochs,
+                        verbose = verbose,
+                        validation_data = (X_validation, y_validation),
+                        batch_size = batch_size)
+    return history
 
 
-# ### Defining a baseline model
+# ## Main
+if __name__ == "__main__":
+    
+    df = load_dataset(TRAINSET_FILE)
+    df_test = load_dataset(TESTSET_FILE)
+    print("Total of training documents: ", df.shape[0])
+    print('Total of testing documents: ',df_test.shape[0])
 
-# Training data
-word_sequence(new_documents)
-X_train = embedding_matrix_train
-X_train_validation = X_train[8000:]
-X_train = X_train[:8000]
+    corpus = concat_text(df)
+    corpus_test = concat_text(df_test)
 
-y_train = documents['infons.relevant'].values
-y_train_validation = y_train[8000:]
-y_train = y_train[:8000]
+    print("Total of training documents after concatenation: ", corpus.shape[0])
+    print("Total of testing documents after concatenation: ", corpus_test.shape[0])
 
-# Test data
-word_sequence(new_documents_test)
-X_test = embedding_matrix_test
-y_test = documents_test['infons.relevant'].values
+    print("Max length for training docs:",max(corpus.astype('str').applymap(lambda x: len(x)).max()))
+    print("Max length for test docs:",max(corpus_test.astype('str').applymap(lambda x: len(x)).max()))
+    maxlen = 3900
+    print("Max lenght for all docs after padding: ", maxlen)
 
-# Check matrix
-print(X_train.shape)
-print(X_train_validation.shape)
 
-# vocab_size = len(np.unique(X_train_batch)) + len(np.unique(X_test_batch))
-vocab_size = len(wv_from_bin.vocab)
-embedding_dim = len(wv_from_bin['the'])
-# maxlen = 3559
+    validation_size = int(corpus.shape[0]*0.1)
+    print("Validation size:",int(validation_size))
+    
 
-print('Vocab size: ', vocab_size)
-print('Embedding dimensions: ', embedding_dim)
-print('Document size ', maxlen)
+    embedding_matrix_train = word_sequence(corpus, (corpus.shape[0], maxlen))
+    embedding_matrix_test = word_sequence(corpus_test, (corpus_test.shape[0], maxlen))
+    X_train, X_validation, X_test, y_train, y_validation, y_test = train_test_split(embedding_matrix_train,
+                                                                                    embedding_matrix_test,
+                                                                                    corpus,
+                                                                                    corpus_test,
+                                                                                    validation_size=validation_size)
+    print('Training set size: ', X_train.shape[0])
+    print('Training targets set size: ', y_train.shape[0])
+    print('Validation set size: ', X_validation.shape[0])
+    print('Validation target set size: ', y_validation.shape[0])
+    print('Test set size: ', X_test.shape[0])
+    print('Test targets set size: ', y_test.shape[0])
 
-# # ### Keras embedding layer
+    wv_from_bin = load_pretrained_w2v(W2V_FILE, W2V_LIMIT)
+    vocab_size = len(wv_from_bin.vocab)
+    embedding_dim = len(wv_from_bin['the'])
+    print('Vocab size:', vocab_size)
+    print('Embedding dimensions:', embedding_dim)
 
-from keras.models import Model
-from keras import layers
+    # #### Keras Model
+    
+    # Getting the embedding layer
+    w2v_embedding = wv_from_bin.get_keras_embedding()
 
- # Getting the embedding layer
-w2v_embedding = wv_from_bin.get_keras_embedding()
+    model = build_compile_model_Dense((maxlen,))
 
-np.random.seed(42)
+    # #### Model fitting and accuracy
 
-# Create the model
-inputs = layers.Input(shape=(3900,))
-embedding = w2v_embedding(inputs)
-flatten = layers.Flatten()(embedding)
-dense = layers.Dense(10, activation = 'relu')(flatten)
-output = layers.Dense(1, activation = 'sigmoid')(dense)
+    history = fit_model(model, X_train, y_train, X_validation, y_validation, epochs=1, batch_size=maxlen, verbose=True)
+    loss, accuracy = model.evaluate(X_train, y_train, verbose = False)
+    print("Training Accuracy: {:.4f}".format(accuracy))
+    loss, accuracy = model.evaluate(X_test, y_test, verbose = False)
+    print("Testing Accuracy:  {:.4f}".format(accuracy))
+    plot_history(history, SAVE_FIG_FILE)
 
-model = Model(inputs=inputs, outputs=output)
-
-model.compile(optimizer = 'adam',
-              loss = 'binary_crossentropy',
-              metrics = ['accuracy'])
-model.summary()
-
-### Model fitting and accuracy
-
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-def plot_history(history):
-     acc = history.history['acc']
-     val_acc = history.history['val_acc']
-     loss = history.history['loss']
-     val_loss = history.history['val_loss']
-     x = range(1, len(acc) + 1)
-
-     plt.figure(figsize=(12, 5))
-     plt.subplot(1, 2, 1)
-     plt.plot(x, acc, 'b', label='Training acc')
-     plt.plot(x, val_acc, 'r', label='Validation acc')
-     plt.title('Training and validation accuracy')
-     plt.legend()
-     plt.subplot(1, 2, 2)
-     plt.plot(x, loss, 'b', label='Training loss')
-     plt.plot(x, val_loss, 'r', label='Validation loss')
-     plt.title('Training and validation loss')
-     plt.legend()
-     plt.savefig('result.png')
-
-history = model.fit(X_train, y_train,
-                     epochs = 150,
-                     verbose = False,
-                     validation_data = (X_train_validation, y_train_validation),
-                    #  validation_split = 0.1,
-                     batch_size = maxlen)
-
-loss, accuracy = model.evaluate(X_train, y_train, verbose = False)
-print("Training Accuracy: {:.4f}".format(accuracy))
-loss, accuracy = model.evaluate(X_test, y_test, verbose = False)
-print("Testing Accuracy:  {:.4f}".format(accuracy))
-plot_history(history)
-
-# predict_output_file(model, documents_test):
