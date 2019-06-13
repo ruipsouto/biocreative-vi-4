2738b5d04f91656a2e5576d7065f123cc8819f2b
Rafael Magalhaes <rafaelmagftw@gmail.com>
2019-06-13 17:59:28 +0100

    nova forma de parsing dos documentos.

--- triage-task.py
+++ triage-task.py
@@ -16,6 +16,8 @@ Reduzir o numero máximo de tokens num documento:
 
 import os, sys
 
+import bioc
+
 import gensim
 from gensim.models import KeyedVectors
 
@@ -38,8 +40,9 @@ plt.style.use('ggplot')
 MODE = 'local'
 
 W2V_FILE = 'PubMed-and-PMC-w2v.bin'
-TRAINSET_FILE = 'PMtask_Triage_TrainingSet.json'
-TESTSET_FILE = 'PMtask_Triage_TestSet.json'
+TRAINSET_FILE = 'PMtask_Triage_TrainingSet.xml'
+TESTSET_FILE = 'PMtask_Triage_TestSet.xml'
+GOLD_STANDARD_FILE = 'PMtask_Triage_TestSet.json'
 EVALSET_FILE = 'Predictions.json'
 SAVE_FIG_FILE = None
 USE_EVALUATE_SCRIPT = True
@@ -74,81 +77,71 @@ def load_pretrained_w2v(file, limit_words=None):
                                                                   binary = True)
     return wv_from_bin
     
-def load_dataset(file):
-#     Import file
-    with open(file) as json_file:
-        data = json.load(json_file)
-    documents = json_normalize(data['documents'])
-#     Unpack the passages column into a standalone dataframe
-    passages = json_normalize(data = data['documents'], record_path = 'passages', record_prefix = 'passage.', meta = 'id')
-#     Unpack the passage.infons column into a standalone dataframe
-    types = json_normalize(passages['passage.infons'], meta = 'id')
-#     Merge
-    documents = documents.merge(passages, on = "id", how = "inner")
-    documents = documents.merge(types, left_index = True, right_index = True)
-    documents = documents.drop(columns = ['passages', 'relations', 'passage.annotations', 'passage.infons', 'passage.relations', 'passage.sentences'])
-    documents['infons.relevant'].replace('no', 0, inplace = True)
-    documents['infons.relevant'].replace('yes', 1, inplace = True)
-    return documents
+def parse_dataset(filename):
+    ids = []
+    titles = []
+    abstracts = []
+    labels = []
+    
+    with bioc.BioCXMLDocumentReader(filename) as reader:
+        collection_info = reader.get_collection_info()
+        for document in reader:
+            ids.append(document.id)
+            relevant = document.infons['relevant']
+            labels.append(0 if relevant == 'no' else 1)
+            titles.append(document.passages[0].text)
+            try:
+                abstracts.append(document.passages[1].text)
+            except IndexError:
+                abstracts.append('')
+                
+        return ids, titles, abstracts, labels
+    
+def concat_text(titles, abstracts):
+    texts = []
+    
+    for i in range(0, len(titles)):
+        text = titles[i] + abstracts[i]
+        texts.append(text)
+        
+    return texts
 
-def concat_text(dataframe):
-    d = {
-        'id':[],
-        'passage.text':[],
-        'infons.relevant':[]
-    }
-    title_count = 0
-    abst_count = 0
-    last_read_row = None
-
-    for index, row in dataframe.iterrows():
-        if row['type'] == 'title':
-            if index > 0:
-                if(last_read_row['type'] == 'title'):
-                    d['id'].append(last_read_row['id'])
-                    d['passage.text'].append(last_read_row['passage.text'])
-                    d['infons.relevant'].append(last_read_row['infons.relevant'])
-            temp = ''
-            temp += row['passage.text']
-            title_count += 1
-            last_read_row = row
-        else:
-            temp += row['passage.text']
-            d['id'].append(row['id'])
-            d['passage.text'].append(temp)
-            d['infons.relevant'].append(row['infons.relevant'])
-            abst_count += 1
-            last_read_row = row
-
-    print("Number of titles encountered:",title_count)
-    print("Number of abstracts encountered:",abst_count)
-    df = pd.DataFrame(data=d)
-    return df
+def get_max_sequence_length(texts, texts_test):
+    max_sequence_training = len(max(texts, key = len))
+    max_sequence_testing = len(max(texts_test, key = len))
+    maxlen = max_sequence_training if max_sequence_training > max_sequence_testing else max_sequence_testing
+    
+    return maxlen
 
 # Text to word sequence
-def vectorize(row, text, embedding_matrix):
+def vectorize(row, text, text_sequences):
     for index, word in enumerate(text):
         try:
-            embedding_matrix[row][index] = wv_from_bin.vocab[word].index
+            text_sequences[row][index] = wv_from_bin.vocab[word].index
         except:
             pass
 
-def word_sequence(df, shape):
-    embedding_matrix = np.zeros(shape, dtype='int',)
-    temp_df = pd.DataFrame()
-    temp_df['passage.text'] = df['passage.text'].apply(lambda x: text_to_word_sequence(x, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = False, split=' '))
-    for index, row in temp_df.iterrows():
-        vectorize(index, row['passage.text'], embedding_matrix)
-    return embedding_matrix
+def word_sequence(texts, shape):
+    text_sequences = np.zeros(shape, dtype='int')
+    temp = [text_to_word_sequence(x, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = False, split=' ') for x in texts]
+    for index, line in enumerate(temp):
+        vectorize(index, line, text_sequences)
+    return text_sequences
 
-def data_split(train_embedding, test_embedding, corpus, corpus_test, validation_size=0):
-    # Training data
-    X_train = train_embedding
-    y_train = corpus['infons.relevant'].values
-    
-    # Test data
-    X_test = test_embedding
-    y_test = corpus_test['infons.relevant'].values
+def data_split(train_sequences, test_sequences, labels, labels_test, validation_size=0, shuffle=False):
+    if(shuffle):
+        indices = np.arange(len(train_sequences))
+        np.random.shuffle(indices)
+        X_train = np.array(train_sequences[indices])
+        y_train = np.array(labels[indices])
+    else:
+        # Training data
+        X_train = np.array(train_sequences)
+        y_train = np.array(labels)
+        
+        # Test data
+        X_test = np.array(test_sequences)
+        y_test = np.array(labels_test)
     
     if(validation_size > 0):
         # Training data
@@ -239,15 +232,15 @@ def fit_model(model, X_train, y_train, X_validation, y_validation, epochs, batch
                         batch_size = batch_size)
     return history
 
-def save_predictions(model, corpus_test, test_sequence):
+def save_predictions(model, ids_test, test_sequence):
     data = {}
     documents = []
     # predict_count = 0
     predictions = model.predict(test_sequence)
-    for index, row in corpus_test.iterrows():
+    for index, id in enumerate(ids_test):
         doc = {}
         infons = {}
-        doc['id'] = row['id']
+        doc['id'] = id
         infons['relevant'] = 'no' if predictions[index] <= 0.5 else 'yes'
         doc['infons'] = infons
         documents.append(doc)
@@ -281,37 +274,40 @@ def debug_embedding(input_shape, w2v_embedding, test_embedding, X_test):
 # ## Main
 if __name__ == "__main__":
 
-    df = load_dataset(TRAINSET_FILE)
-    df_test = load_dataset(TESTSET_FILE)
-    print("Total of training documents: ", df.shape[0])
-    print('Total of testing documents: ',df_test.shape[0])
+    # Load training and testing dataset
+    print('-------------------DOC PARSING--------------------')
+    ids, titles, abstracts, labels = parse_dataset(TRAINSET_FILE)
+    texts = concat_text(titles, abstracts)
+    ids_test, titles_test, abstracts_test, labels_test = parse_dataset(TESTSET_FILE)
+    texts_test = concat_text(titles_test, abstracts_test)
 
-    corpus = concat_text(df)
-    corpus_test = concat_text(df_test)
+    MAX_SEQUENCE_LENGTH = get_max_sequence_length(texts, texts_test)
+    
+    print("Total of training documents: ", len(texts))
+    print('Total of testing documents: ',len(texts_test))
+    print('Max Sequence Length:', MAX_SEQUENCE_LENGTH)
+    print('')
 
-    print("Total of training documents after concatenation: ", corpus.shape[0])
-    print("Total of testing documents after concatenation: ", corpus_test.shape[0])
-
-    print("Max length for training docs:",max(corpus.astype('str').applymap(lambda x: len(x)).max()))
-    print("Max length for test docs:",max(corpus_test.astype('str').applymap(lambda x: len(x)).max()))
-    maxlen = 3900
-    print("Max lenght for all docs after padding: ", maxlen)
-
+    print('-----------------LOADING EMBEDDING----------------')
+    # Load of the pre trained embeddings
     wv_from_bin = load_pretrained_w2v(W2V_FILE, W2V_LIMIT)
     vocab_size = len(wv_from_bin.vocab)
     embedding_dim = len(wv_from_bin['the'])
     print('Vocab size:', vocab_size)
     print('Embedding dimensions:', embedding_dim)
+    print('')
+
+    print('-------------------PRE PROCESSING-------------------')
     
-    validation_size = int(corpus.shape[0]*0.1)
+    validation_size = int(len(texts)*0.1)
     print("Validation size:",int(validation_size))
 
-    train_word_sequence = word_sequence(corpus, (corpus.shape[0], maxlen))
-    test_word_sequence = word_sequence(corpus_test, (corpus_test.shape[0], maxlen))
+    train_word_sequence = word_sequence(texts, (len(texts), MAX_SEQUENCE_LENGTH))
+    test_word_sequence = word_sequence(texts_test, (len(texts_test), MAX_SEQUENCE_LENGTH))
     X_train, X_validation, X_test, y_train, y_validation, y_test = data_split(train_word_sequence,
                                                                                 test_word_sequence,
-                                                                                corpus,
-                                                                                corpus_test,
+                                                                                labels,
+                                                                                labels_test,
                                                                                 validation_size=validation_size)
     print('Training set size: ', X_train.shape[0])
     print('Training targets set size: ', y_train.shape[0])
@@ -319,20 +315,22 @@ if __name__ == "__main__":
     print('Validation target set size: ', y_validation.shape[0])
     print('Test set size: ', X_test.shape[0])
     print('Test targets set size: ', y_test.shape[0])
+    print('')
 
+    print('-------------------TRAINING MODEL-------------------')
     # #### Keras Model
     
     # Getting the embedding layer
     w2v_embedding = wv_from_bin.get_keras_embedding()
 
     if MODEL_ARC == 'dense':
-        model = build_compile_model_Dense((maxlen,), w2v_embedding)
+        model = build_compile_model_Dense((MAX_SEQUENCE_LENGTH,), w2v_embedding)
     elif MODEL_ARC == 'lstm':
-        model = build_compile_model_LSTM((maxlen,), w2v_embedding)
+        model = build_compile_model_LSTM((MAX_SEQUENCE_LENGTH,), w2v_embedding)
     elif MODEL_ARC == 'lstm-gpu':
-        model = model_lstm_du((maxlen,), w2v_embedding)
+        model = model_lstm_du((MAX_SEQUENCE_LENGTH,), w2v_embedding)
     elif MODEL_ARC == 'debug-embedding':
-        debug_embedding((maxlen,), w2v_embedding, test_word_sequence, X_test)
+        debug_embedding((MAX_SEQUENCE_LENGTH,), w2v_embedding, test_word_sequence, X_test)
         sys.exit(0)
     
     model.summary()
@@ -341,6 +339,11 @@ if __name__ == "__main__":
     # #### Model fitting and accuracy
 
     history = fit_model(model, X_train, y_train, X_validation, y_validation, epochs=EPOCHS, batch_size=BATCHSIZE, verbose=1)
+
+
+    print('')
+
+    print('--------------------MODEL EVALUATION-------------------')
 
     loss, accuracy = model.evaluate(X_train, y_train, verbose = False)
     print("Training Accuracy: {:.4f}".format(accuracy))
@@ -352,6 +355,7 @@ if __name__ == "__main__":
     
     if(USE_EVALUATE_SCRIPT):
         print('---------------------EVALUATION SCRIPT OUTPUT---------------------------')
-        save_predictions(model, corpus_test, test_word_sequence)
-        os.system('python eval_json.py triage ' + TESTSET_FILE + ' ' + EVALSET_FILE)
-        print('--------------------------END OF OUTPUT---------------------------------')
+        save_predictions(model, ids_test, test_word_sequence)
+        os.system('python eval_json.py triage ' + GOLD_STANDARD_FILE + ' ' + EVALSET_FILE)
+
+    print('---------------------------END---------------------------------')
